00d98413323033c0d128fa2baacfc8afc6d7789b
kr1viah_ <85935121+kr1viah@users.noreply.github.com>
2024-04-10 19:20:12 +0200

    Delete mods-unpacked/kr1v-autosplitter/extensions/src/element/power_token directory

--- mods-unpacked/kr1v-autosplitter/extensions/src/element/power_token/powerToken.gd
+++
@@ -1,6 +0,0 @@
-extends "res://src/element/power_token/powerToken.gd"
-
-# This overrides the method with the same name, changing the value of its argument:
-func hit(body:Node2D):
-	print_debug("i'm pwoer token lol")
-	super.hit(body)
